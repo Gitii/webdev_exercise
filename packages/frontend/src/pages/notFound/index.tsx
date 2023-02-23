96db9c0a27389fa78d83cd73cd46f53d107ded75
Gitii <Gitii@users.noreply.github.com>
2023-02-23 17:42:35 +0100

    Updated notFound page and tests

--- packages/frontend/src/pages/notFound/index.tsx
+++ packages/frontend/src/pages/notFound/index.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import ErrorView from '../../components/ErrorView';
 
 export default function () {
@@ -7,9 +7,6 @@ export default function () {
   return (
     <div>
       <ErrorView error={`Unknown route ${location.pathname}`} />
-      <p>
-        <Link to="/">Go back to home</Link>
-      </p>
     </div>
   );
 }
